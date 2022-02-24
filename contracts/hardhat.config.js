671e51a433691542e649769e95469be8160ced73
LottyZ <lotty.lingyu@gmail.com>
2022-02-24 19:28:51 +0800

    Update configuration

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -4,38 +4,48 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
-const { RINKEBY_API_URL, ROPSTEN_API_URL, POLYGON_API_URL, MUMBAI_API_URL, MAINNET_API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+const {
+   RINKEBY_API_URL,
+   ROPSTEN_API_URL,
+   POLYGON_API_URL,
+   MAINNET_API_URL,
+   PUBLISHER_PRIVATE_KEY,
+   API_KEY
+} = process.env;
 module.exports = {
-   solidity: "0.8.0",
-   defaultNetwork: "rinkeby",
+   solidity: "0.8.1",
+   defaultNetwork: "mumbai",
    networks: {
       hardhat: {},
       rinkeby: {
          url: RINKEBY_API_URL,
-         accounts: [`0x${PRIVATE_KEY}`],
+         accounts: [PUBLISHER_PRIVATE_KEY],
          gasPrice: 1000000000, // 1 gwei
       },
       mainnet: {
          url: MAINNET_API_URL,
-         accounts: [`0x${PRIVATE_KEY}`],
+         accounts: [PUBLISHER_PRIVATE_KEY],
          gasPrice: 55000000000, // 55 gwei
       },
       polygon: {
          url: POLYGON_API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+         accounts: [PUBLISHER_PRIVATE_KEY]
       },
       mumbai: {
-         url: MUMBAI_API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+         url: POLYGON_API_URL,
+         accounts: [PUBLISHER_PRIVATE_KEY]
       },
       ropsten: {
          url: ROPSTEN_API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+         accounts: [PUBLISHER_PRIVATE_KEY]
       }
    },
    etherscan: {
       // Your API key for Etherscan
       // Obtain one at https://etherscan.io/
-      apiKey: ETHERSCAN_API_KEY,
-    }
+      apiKey: {
+         polygon: API_KEY,
+         polygonMumbai: API_KEY
+      }
+   }
 }
